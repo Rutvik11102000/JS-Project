f53d2cb93543df8e5d9d5ab18337b1ac947bd284
Rutvik11102000 <101519046+Rutvik11102000@users.noreply.github.com>
2022-12-14 11:23:16 +0530

    Assignment
    Number System Converter by using HTML, CSS and JavaScript.

--- script.js
+++ script.js
@@ -1,84 +1,29 @@
-//JavaScript Code
-//===============
-//function number system conversion
-function convert() {
-    var iNum = document.getElementById("ipNum").value;
-    var iType = document.getElementById("ipType").value;
-    var oType = document.getElementById("opType").value;
-    var oNum;
+const from = document.getElementById("from");
+const to = document.getElementById("to");
+const inputNumber = document.getElementById("input-number");
+const resultBox = document.getElementById("result-box");
 
-    //conversion from binary to oct, dec, hex
-    if (iType == "from-binary" && oType == "to-octal") oNum = bin2oct(iNum);
-    if (iType == "from-binary" && oType == "to-decimal") oNum = bin2dec(iNum);
-    if (iType == "from-binary" && oType == "to-hexadecimal") oNum = bin2hex(iNum);
-    if (iType == "from-binary" && oType == "to-binary") oNum = "INVALID";
-    //conversion from decimal to bin, oct, hex
-    if (iType == "from-decimal" && oType == "to-binary") oNum = dec2bin(iNum);
-    if (iType == "from-decimal" && oType == "to-octal") oNum = dec2oct(iNum);
-    if (iType == "from-decimal" && oType == "to-hexadecimal") oNum = dec2hex(iNum);
-    if (iType == "from-decimal" && oType == "to-decimal") oNum = "INVALID";
-    //conversion from octal to bin, dec, hex
-    if (iType == "from-octal" && oType == "to-binary") oNum = oct2bin(iNum);
-    if (iType == "from-octal" && oType == "to-decimal") oNum = oct2dec(iNum);
-    if (iType == "from-octal" && oType == "to-hexadecimal") oNum = oct2hex(iNum);
-    if (iType == "from-octal" && oType == "to-octal") oNum = "INVALID";
-    //conversion from hexadecimal to bin, dec, oct
-    if (iType == "from-hexadecimal" && oType == "to-binary") oNum = hex2bin(iNum);
-    if (iType == "from-hexadecimal" && oType == "to-octal") oNum = hex2oct(iNum);
-    if (iType == "from-hexadecimal" && oType == "to-decimal") oNum = hex2dec(iNum);
-    if (iType == "from-hexadecimal" && oType == "to-hexadecimal") oNum = "INVALID";
+document.getElementById("convert").addEventListener("click", () => {
+  //check from
+  let fromValue = from.value;
+  //check to
+  let toValue = to.value;
+  //check input value
+  let inputValue = inputNumber.value;
+  //convert number to 'from' to 'to'
+  let result = convertBase(fromValue, toValue, inputValue);
+  //show result in result box
+  resultBox.innerText = result;
+});
+function convertBase(fromBase, toBase, num) {
+  let d = parseInt(num, fromBase); // converting number into base10
+  let res = d.toString(toBase); // converting base10 to required base
+  return res;
+}
 
-    document.getElementById("opNum").value = oNum;
-}
-//=============================================================
-//function binary to decimal conversion
-function bin2dec(iNum) {
-    return parseInt(iNum, 2);
-}
-//function binary to octal conversion
-function bin2oct(iNum) {
-    return parseInt(iNum, 2).toString(8);
-}
-//function binary to hexadecimal conversion
-function bin2hex(iNum) {
-    return parseInt(iNum, 2).toString(16);
-}
-//===========================================
-//function decimal to binary conversion
-function dec2bin(iNum) {
-    return parseInt(iNum).toString(2);
-}
-//function decimal to octal conversion
-function dec2oct(iNum) {
-    return parseInt(iNum).toString(8);
-}
-//function decimal to hexadecimal conversion
-function dec2hex(iNum) {
-    return parseInt(iNum).toString(16);
-}
-//===========================================
-//function octal to binary conversion
-function oct2bin(iNum) {
-    return parseInt(iNum, 8).toString(2);
-}
-//function octal to decimal conversion
-function oct2dec(iNum) {
-    return parseInt(iNum, 8).toString(10);
-}
-//function octal to hexadecimal conversion
-function oct2hex(iNum) {
-    return parseInt(iNum, 8).toString(16);
-}
-//===========================================
-//function hexadecimal to binary conversion
-function hex2bin(iNum) {
-    return parseInt(iNum, 16).toString(2);
-}
-//function hexadecimal to octal conversion
-function hex2oct(iNum) {
-    return parseInt(iNum, 16).toString(8);
-}
-//function hexadecimal to decimal conversion
-function hex2dec(iNum) {
-    return parseInt(iNum, 16).toString(10);
-}
+// swapping
+document.getElementById("swap").addEventListener("click", () => {
+  let temp = from.value;
+  from.value = to.value;
+  to.value = temp;
+});
